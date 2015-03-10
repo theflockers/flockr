c96356425a4537db4d039acf6b4cfa4720dc54f4
Leandro Mendes <leandro.mendes@dafiti.com.br>
2015-03-10 16:56:06 -0300

    funcoes para build: create_archive, clone_repository, etc

--- lib/flockr/control/__init__.py
+++ lib/flockr/control/__init__.py
@@ -3,7 +3,7 @@
 from Queue import Queue
 from threading import Thread
 from termcolor import colored
-from subprocess import Popen, PIPE
+from subprocess import Popen, call, PIPE
 
 from libcloud.compute.types import Provider
 from libcloud.compute.providers import get_driver
@@ -31,8 +31,8 @@ class Control:
       tar = tarfile.open(self.cfg.get('build')['base_url'])
       root_fs = '%s/root-fs' % (self.tmp_build_dir)
       try:
-        print colored('=> Cleaning up tmpdir %s' % (root_fs), 'yellow')
-        shutil.rmtree(root_fs)
+        print colored('=> Cleaning up tmpdir %s' % (self.tmp_build_dir), 'yellow')
+        shutil.rmtree(self.tmp_build_dir)
         os.makedirs(root_fs)
       except Exception, e:
         print str(e)
@@ -44,9 +44,35 @@ class Control:
     app_dir = '%s/app' % self.tmp_build_dir
     print colored('=> Fetching application from %s' % (self.cfg.get('build')['application_repository']), 'yellow')
     p = Popen(['/usr/bin/git','clone', self.cfg.get('build')['application_repository'], app_dir], stderr=PIPE, stdout=PIPE)
+    p.wait()
     res = p.communicate()
-    if len(res[0]) == 0:
+    if len(res[1]) != 0:
       print colored('=> ERROR:', 'yellow'), colored('%s' % ( res[1] ), 'red')
+    else:
+      print colored('=> %s ' % (res[0].strip()), 'yellow')
+
+  def merge_application(self):
+    wwwroot = '%s/root-fs/%s' % ( self.tmp_build_dir, self.cfg.get('build')['application_wwwroot'] )
+    srcdir  = '%s/app/' % (self.tmp_build_dir)
+    print colored('=> Merging %s with base s.o. into %s' % (srcdir, wwwroot),'yellow')
+    try:
+      shutil.rmtree(wwwroot)
+    except OSError, e:
+      print colored('=> WARNING:', 'yellow'), colored('%s' % ( str(e) ), 'red')
+
+    shutil.copytree(srcdir, wwwroot)
+
+  def create_archive(self):
+    print colored('=> Creating archive %s/app-%s.tar' % (self.name, self.name),'yellow')
+    tar = tarfile.open('%s/app-%s.tar' %  (self.name, self.name), 'w:' )
+    for root, dirs, files in os.walk('%s/root-fs' % self.tmp_build_dir ):
+      for f in files:
+        filepath =  "%s/%s" % (root,f)
+        arcname =  re.sub('%s/root-fs/' % self.tmp_build_dir, '', filepath)
+        if re.search('.git\/', root):
+          continue
+        tar.add('%s/%s' % (root, f), arcname = arcname)
+    tar.close()
 
   def create(self, node): pass
 
@@ -54,14 +80,19 @@ class Control:
 
   def build(self, name):
     # opening config
-    self.cfg = config.Config('%s/config.yaml' % (name) )
+    self.name = name
+    self.cfg = config.Config('%s/config.yaml' % (self.name) )
 
     self.tmp_build_dir = '%s/%s'  % (self.cfg.get('build')['tmpdir'], \
         self.__BUILD_DIRECTORY_NAME)
 
-    print colored( '\n* Building app %s *\n' % name, 'yellow')
+    print colored( '\n* Building app %s *\n' % self.name, 'yellow')
     self.download_base_system()
     self.clone_application()
+    self.merge_application()
+    self.create_archive()
+
+    print colored('\n+++ Build done. Now you may want register a template +++\n', 'green')
 
   def register(self): pass
 
@@ -69,7 +100,7 @@ class Control:
     try:
       os.mkdir(name)
       f = open('%s/config.yaml' % (name), 'w')
-      f.write(open('lib/flockr/example/config.yaml','r').read())
+      f.write(open('lib/flockr/example/config.yaml-example','r').read())
       f.close()
     except Exception, e: pass
 
